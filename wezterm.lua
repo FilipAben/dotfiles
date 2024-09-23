70e065f2caab8fade26b9159beb7dfa5dd775691
Filip Aben <faben@isabelgroup.eu>
2024-09-23 13:36:06 +0200

    Change wezterm color scheme

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Belge'
+config.color_scheme = 'Belge (terminal.sexy)'
 config.font = wezterm.font 'BlexMono Nerd Font'
 config.scrollback_lines = 100000
 config.font_size = 16
